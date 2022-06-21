a3e8ea8cc12c1039349f1d9725993b3ac06b6d53
Matthew Feickert <matthew.feickert@cern.ch>
2022-06-21 18:40:28 -0500

    Disable RooFit banner

--- v6.26.04/build_ROOT.sh
+++ v6.26.04/build_ROOT.sh
@@ -17,29 +17,35 @@ if [ -d "root_build_${ROOT_VERSION}" ]; then
 fi
 
 # c.f. https://root.cern/install/build_from_source/#all-build-options
+INSTALL_PREFIX="${HOME}/bin/root-cern"
 cmake \
     -Dall=OFF \
     -Dsoversion=ON \
     -Dgsl_shared=ON \
     -DCMAKE_CXX_STANDARD=17 \
+    -Droot7=ON \
     -Dfortran=ON \
     -Droofit=ON \
     -Droostats=ON \
     -Dhistfactory=ON \
     -Dminuit2=ON \
-    -Dbuiltin_xrootd=ON \
+    -DCMAKE_CXX_FLAGS=-D__ROOFIT_NOBANNER \
     -Dxrootd=ON \
+    -Dbuiltin_xrootd=ON \
     -Dpyroot=ON \
     -Dqtgsi=OFF \
     -Dcuda=OFF \
     -Dtmva-gpu=OFF \
-    -Droot7=ON \
     -DPYTHON_EXECUTABLE="$(pyenv which python)" \
-    -DCMAKE_INSTALL_PREFIX="${HOME}/bin/root-cern" \
+    -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
     -S root_src \
     -B "root_build_${ROOT_VERSION}"
 cmake "root_build_${ROOT_VERSION}" -LH
 cmake --build "root_build_${ROOT_VERSION}" -- -j$(($(nproc) - 1))
+
+if [ -d "${INSTALL_PREFIX}" ]; then
+    rm -rf "${INSTALL_PREFIX}"
+fi
 cmake --build "root_build_${ROOT_VERSION}" --target install
 
 unset ROOT_VERSION
